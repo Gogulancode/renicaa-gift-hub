c16fd1256eab96224d698563bfb01d9d9ad20070
Solutionsnextwave <gogulan@solutionsnext.in>
2025-05-31 13:03:56 +0530

    Resolve merge conflict in Index.tsx and accept current changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 944bf0a80c2e8e9c92a562be37cd27e077726548
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Features from '@/components/Features';
@@ -9,14 +5,11 @@ import Testimonials from '@/components/Testimonials';
 import Brands from '@/components/Brands';
 import Products from '@/components/Products';
 import Footer from '@/components/Footer';
-<<<<<<< HEAD
 import Instagram from '@/components/Instagram';
 import FestiveGift from '@/components/FestiveGift';
 import OurPicks from '@/components/OurPicks';
 import DownloadCatalogue from '@/components/DownloadCatalogue';
 import HowItWorks from '@/components/HowItWorks';
-=======
->>>>>>> 944bf0a80c2e8e9c92a562be37cd27e077726548
 
 const Index = () => {
   return (
@@ -26,17 +19,12 @@ const Index = () => {
         <Hero />
         <Features />
         <Brands />
-<<<<<<< HEAD
         <HowItWorks />
         <FestiveGift />
         <OurPicks />
         <DownloadCatalogue />
         <Testimonials />
         <Instagram />
-=======
-        <Products />
-        <Testimonials />
->>>>>>> 944bf0a80c2e8e9c92a562be37cd27e077726548
       </main>
       <Footer />
     </div>
