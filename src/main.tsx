e2391751bfc3d37acaf6ef1496f9a0da3f2e205b
Solutionsnextwave <gogulan@solutionsnext.in>
2025-05-31 13:34:57 +0530

    Fix: clean up main.tsx and add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-=======
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById("root")!).render(<App />);
->>>>>>> 944bf0a80c2e8e9c92a562be37cd27e077726548
